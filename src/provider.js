4ba6e072692ccd29ad08a01e7fd5b277912db3f9
Scott Ames-Messinger <scott@commoncurriculum.com>
2019-04-30 13:27:27 -0400

    Try entity?

--- src/provider.js
+++ src/provider.js
@@ -3,7 +3,7 @@
 const completions = require('./completions.json');
 
 module.exports = {
-  selector: '.string.quoted, .source.pug .constant.language.js, source.css, source.css.scss, source.css.less, .css, .scss, .property-name.scss',
+  selector: '.string.quoted, .source.pug .constant.language.js, source.css, source.css.scss, source.css.less, .css, .scss, .entity.scss',
 
   getSuggestions (request) {
     const { prefix, bufferPosition, editor, scopeDescriptor } = request;
